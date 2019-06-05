b029e7d9d1c97d0a159192349cebdcf873924ab5
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-06-05 15:35:35 -0600

    layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'tachyons';
 
 class App extends React.Component {
     constructor(props) {
@@ -7,8 +8,18 @@ class App extends React.Component {
     }
     render() {
         return(
-            <div>
-                hello
+            <div
+                className="min-vh-100 flex flex-column items-center bg-green"
+            >
+              <h1>Pomodoro Clock</h1>
+              <div
+                className="flex justify-center"
+              >
+                <div>break length</div>
+                <div>session length</div>
+              </div>
+              <div>session</div>
+              <div>buttons</div>
             </div>
         );
     }
