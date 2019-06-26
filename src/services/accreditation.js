19bb81faca28d4e37ac2b7a3a6cc55d920427517
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2019-06-26 11:47:41 -0600

    adding ui improvements

---
+++ src/services/accreditation.js
@@ -0,0 +1,3 @@
+// link for arrow https://fontawesome.com/icons/arrow-circle-up?style=solid 
+// link for arrow https://fontawesome.com/icons/arrow-circle-down?style=solid
+// link for refresh https://fontawesome.com/icons/sync?style=solid
